9c834c8fa8e4315f2051f530f902f2ba1891ea5f
Sabrina-Aguirre <68922833+Sabrina-Aguirre@users.noreply.github.com>
2025-08-24 23:21:56 -0300

    Create producto.py
    Se creo la clase ProductoORM. Sabrina Aguirre

---
+++ backend/app/db/models/producto.py
@@ -0,0 +1,8 @@
+
+class ProductoORM:
+    def __init__(self, nombre, SKU, categoria, descripcion, unidad_medida):
+        self.nombre = nombre
+        self.SKU = SKU
+        self.categoria = categoria
+        self.descripcion = descripcion
+        self.unidad_medida = unidad_medida
