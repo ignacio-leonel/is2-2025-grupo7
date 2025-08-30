6f22dd4432c93239b40593115f225c3335d18f8f
Bicerne Javier <leonermails@gmail.com>
2025-08-30 18:55:36 -0300

    Agregar las demas dataclasses del modelo de dominio

--- backend/app/domain/models/producto.py
+++ backend/app/domain/models/producto.py
@@ -1,7 +1,6 @@
-# app/domain/models/producto.py
 from dataclasses import dataclass, field
-from typing import Optional
-
+from typing import Optional,List
+from .movimiento import Movimiento 
 @dataclass
 class Producto:
     id: int
@@ -10,3 +9,4 @@ class Producto:
     categoria: str
     descripcion: Optional[str] = None #Puede no tener valor
     stock_global: int = field(default=0, init=False) #No se pasa en el constructor; es Calculado
+    movimientos: List[Movimiento] = field(default_factory=list) #Relación Producto → Movimiento
